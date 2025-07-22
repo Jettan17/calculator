2688e17184516523e352dc6189573c476671832d
Jettan17 <jethrotan2006@gmail.com>
2025-07-22 17:20:04 +0800

    feat: populate display with numbers

--- script.js
+++ script.js
@@ -27,6 +27,26 @@ function operate(a, b, op) {
     }
 }
 
+function populate(elem, current) {
+    //update display
+    const display = document.querySelector(".display");
+    display.textContent = `${display.textContent}${elem}`;
+
+    //store current num
+    current = `${current}${elem}`;
+    console.log(current);
+
+    return current;
+}
+
 let first = 0;
 let second = 0;
 let operator = "+";
+let current = "";
+let operatorActive = false;
+
+const numberButtons = document.querySelectorAll(".numbers button")
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {populate(button.className, current)});
+});
